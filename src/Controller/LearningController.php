9870272cfc239f60124c345cebc6d95d67dfa87b
Leweyse <darylcastro67@gmail.com>
2022-01-10 15:56:24 +0100

    created route and template for home page, added default value for name

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
@@ -16,11 +19,21 @@ class LearningController extends AbstractController
         ]);
     }
 
-    #[Route('/about-me', name: 'about-me')]
-    public function aboutMe()
+    #[Route('/home', name: 'homepage')]
+    public function showMyName(Request $request): Response
     {
-        return $this->render('learning/about-me.html.twig', [
+        $session = $request->getSession();
+        $session_name = $session->get('user_name');
+
+        if ($session_name) {
+            $name = $session_name;
+        } else {
+            $name = "Unknown";
+        }
+
+        return $this->render('learning/home.html.twig', [
             'controller_name' => 'LearningController',
+            'name' => $name
         ]);
     }
 }
