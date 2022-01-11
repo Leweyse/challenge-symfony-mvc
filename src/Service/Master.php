a397e1d49eb19ef9bb548869c3663a5bffa4142d
Leweyse <darylcastro67@gmail.com>
2022-01-11 14:22:14 +0100

    added type hint and implemented universal method

--- src/Service/Master.php
+++ src/Service/Master.php
@@ -4,18 +4,14 @@ namespace App\Service;
 
 class Master
 {
-    public function __construct(TransformInterface $capitalize, TransformInterface $dashes) {
+    public function __construct(TransformInterface $capitalize, TransformInterface $dashes)
+    {
         $this->capitalize = $capitalize;
         $this->dashes = $dashes;
     }
 
-    public function capitalize(String $string): string
+    public function transformer(String $string, String $transformationClass): string
     {
-        return $this->capitalize->transform($string);
-    }
-
-    public function dashes(String $string): string
-    {
-        return $this->dashes->transform($string);
+        return $this->$transformationClass->transform($string);
     }
 }
